75167ba9a9585c82353c4a1add41a2bcd2124598
mnturingan <119831456+mnturingan@users.noreply.github.com>
2024-10-15 08:57:09 +0000

    to sycn with main

--- laravel-docker/resources/views/welcome.blade.php
+++ laravel-docker/resources/views/welcome.blade.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-<ul id="dashboards" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar" style="">
-		<li class="sidebar-item"><a class="sidebar-link" href="{{url('create_product')}}">
-					<i class="align-middle me-2" data-feather="chevron-right"></i>Make an appointment</a>
-					</li>
-							<li class="sidebar-item"><a class="sidebar-link" href="{{url('view_product')}}">
-								<i class="align-middle me-2" data-feather="chevron-right"></i>BEspoke </a>
-							</li>
-						</ul>
+    test
 </body>
 </html>
