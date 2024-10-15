ba798ad7a5538709b7109c67fc1ada976fae4189
mnturingan <119831456+mnturingan@users.noreply.github.com>
2024-10-15 08:46:54 +0000

    admin login new design

--- laravel-docker/app/Models/Booking.php
+++ laravel-docker/app/Models/Booking.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'appointment_type',
+        'date',
+        'time',
+        'first_name',
+        'last_name',
+        'country',
+        'phone',
+        'email',
+        'preferred_channel',
+        'number_of_people',
+        'additional_info',
+    ];
 }
