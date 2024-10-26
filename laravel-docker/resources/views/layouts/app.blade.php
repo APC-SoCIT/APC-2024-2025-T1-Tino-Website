dfe92b3a3ef5849772cff54fd3d3a32a41281714
addeguzman2 <119831458+addeguzman2@users.noreply.github.com>
2024-10-26 09:13:31 +0800

    Update app.blade.php

--- laravel-docker/resources/views/layouts/app.blade.php
+++ laravel-docker/resources/views/layouts/app.blade.php
@@ -38,6 +38,8 @@
     @yield('content')
 
     @include('layouts.footer')
+    <script src="{{ asset('js/carousel.js') }}"></script>
+    <script src="{{ asset('js/scroll.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
